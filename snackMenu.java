4fe40700706a620e0baa06b85ebecf9a933b8343
Feroxik2007 <kovalikadrian@gmail.com>
2024-11-28 12:44:22 +0100

    Update snackMenu.java

--- snackMenu.java
+++ snackMenu.java
@@ -1,102 +1,109 @@
-
 import java.io.FileWriter;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class snackMenu {
     
-    public static void menuSnackov() throws InterruptedException{
-        int popcorn = 10;
-        int twix = 10;
-        int mms = 10;
-        int snickers = 10;
-        int reese = 10;
-
-        Scanner scanner = new Scanner(System.in);
-        Snack cena = new Snack();
-        cena.setCena(1.8);
-        List<String> pismena = new ArrayList<>();
-        pismena.add("A");
-        pismena.add("B");
-        pismena.add("C");
-        pismena.add("D");
-        pismena.add("E");
-        List<Integer> cisla = new ArrayList<>();
-        cisla.add(1);
-        cisla.add(2);
-        cisla.add(3);
-        cisla.add(4);
-        cisla.add(5);
-        List<String> snacks = new ArrayList<>();
-        snacks.add("Popcorn");
-        snacks.add("Twix");
-        snacks.add("M&M");
-        snacks.add("Snickers");
-        snacks.add("Reese's");
-        System.out.println("Zvolili ste si snacky, tu je nasa ponuka: ");
-        for (int i = 0; i < snacks.size(); i++) {
-            if (i < pismena.size() && i < cisla.size()) {  
-                System.out.println(snacks.get(i) + ": " + pismena.get(i) + cisla.get(i) + " " +  ":  "+ cena.getCena() + " Eur");
-            }
-        }
-        System.out.print("Co si prajete: ");
-        String vyber = scanner.nextLine();
+    public static void main(String[] args) throws InterruptedException {
+        Diddy();
+    }
+    public static void Diddy() throws InterruptedException{
+        Snack snacky = new Snack();
+        snacky.setCena(1.9);
         
+        int snickers = 5;
+        int mms = 5;
+        int reeses = 5;
+        int Kitkat = 5;
+        int twix = 5;
+        Scanner scanner = new Scanner(System.in);
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "Snickers " );
+        map.put(2, "M&M's " );
+        map.put(3, "Reese's Pieces" );
+        map.put(4, "Kit Kat  ");
+        map.put(5, "Twix" );
+        System.out.println("Tu je nase menu: ");
+        System.out.println("Snickers -  [1A]" + "cena: " +snacky.getCena());
+        Thread.sleep(500);
+        System.out.println("M&M's - [2B]" + "cena: " +  snacky.getCena());
+        Thread.sleep(500);
+        System.out.println("Reese's Pieces - [3C]" + "cena: " + snacky.getCena());
+        Thread.sleep(500);
+        System.out.println("Kik Kat - [4D]" + "cena: " + snacky.getCena());
+        Thread.sleep(500);
+        System.out.println("Twix - [5E]"  + "cena: " + snacky.getCena());
+        System.out.println("Co si prajete?");
+        String odpoved = scanner.nextLine();
+
         try {
             FileWriter writer = new FileWriter("snack.txt");
-            if (vyber.equalsIgnoreCase("A1")) {
-                popcorn--;
-                writer.write("Popcorn: " + popcorn + "\n");
-                writer.close();
-                System.out.println("Vybral si si popcorn");
-                platba();
-            }
-            if (vyber.equalsIgnoreCase("B2")) {
-                twix--;
-                writer.write("Twix: " + twix + "\n");
-                writer.close();
-                System.out.println("Vybral si si twix");
-                platba();
-            }
-            if (vyber.equalsIgnoreCase("C3")) {
-                mms--;
-                writer.write("M&M: " + mms + "\n");
-                writer.close();
-                System.out.println("Vybral si si M&M");
-                platba();
-            }
-            if(vyber.equalsIgnoreCase("D4")){
+            if (odpoved.contains("1")) {
                 snickers--;
-                writer.write("Snickers: " + snickers + "\n");
-                writer.close();
-                System.out.println("Vybral si si snickers");
+                writer.write("Pocete snickersu: " + snickers);
+                if (snickers == 0) {
+                    System.out.println("Snickers su vypredane!");
+                    Thread.sleep(1000);
+                    Diddy();
+                }
+                System.out.println( "Vybrali ste si: "+ " "  + map.get(1));
                 platba();
-                
             }
-            if(vyber.equalsIgnoreCase("E5")){
-                reese--;
-                writer.write("Reese's: " + reese + "\n");
-                writer.close();
-                System.out.println("Vybral si si reese's");
+            
+            if(odpoved.contains("2")){
+                mms--;
+                writer.write("Pocete M&M's: " + " " + mms);
+                if (mms == 0) {
+                    System.out.println("M&M's su vypredane");
+                }
+                System.out.println("Vybrali ste si: " + " " + map.get(2));
+                platba();
+            }
+            if(odpoved.contains("3")){
+                reeses--;
+                writer.write("Pocete Reese's Pieces: " + " " + reeses);
+                if (reeses == 0) {
+                    System.out.println("Reese's Pieces su vypredane");
+                }
+                System.out.println( "Vybrali ste si: " + " " + map.get(3));
+                platba();
+            }
+            if(odpoved.contains("4")){
+                Kitkat--;
+                writer.write("Pocete Kit Kat: " + " " + Kitkat);
+                if (Kitkat == 0) {
+                    System.out.println("Kit Kat su vypredane");
+                }
+                System.out.println("Vybrali ste si: " + " " + map.get(4));
+                platba();
+            }
+            if(odpoved.contains("5")){
+                twix--;
+                writer.write("Pocete Twix: " + twix);
+                if (twix == 0) {
+                    System.out.println("Twix su vypredane");
+                }
+                System.out.println("Vybrali ste si" + map.get(5));
                 platba();
             }
             else{
-                System.out.println("Vybral si si neexistujuci produkt");
-                System.out.println("Vyber si este raz");
-                menuSnackov();
+                System.out.println("Nespravna volba");
+                System.out.println("Skus este raz!");
+                Thread.sleep(1000);
+                Diddy();
             }
         } catch (Exception e) {
-            System.out.println("Nespravny vstup");
+            System.out.println("Somehing went wrong");
         }
-        
-        
+       
+        scanner.close();
         
     }
     public static void platba() throws InterruptedException{
         Scanner scanner = new Scanner(System.in);
         Snack cena = new Snack();
-        cena.setCena(1.8);
+        cena.setCena(1.9);
         System.out.println("Mate presne " + cena.getCena() + " Eur ?"  + " ano/nie");
         String vyber = scanner.nextLine();
         try {
@@ -145,4 +152,5 @@ public class snackMenu {
         
         
     }
+    
 }
