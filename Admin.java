6ccd08832ca1b1b1c68b59c7593eae5e81a0c0ce
Feroxik2007 <kovalikadrian@gmail.com>
2024-12-01 19:08:37 +0100

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -9,12 +9,33 @@ public class Admin {
         String prihlasovacieMeno = scanner.next();
         if (prihlasovacieMeno.equals("Coopjednotaprievidza16")) {
             System.out.println("Ste uspesne prihlaseny!");
+            System.out.println("1. Doplnit");
+            System.out.println("2. Odobrat");
+            System.out.println("3. Prejst na hlavne menu");
+            int moznost = scanner.nextInt();
+                if (moznost == 1) {
+                    doplnit();
+                }
+                if (moznost == 2) {
+                    odobrat();
+                }
+                if (moznost == 3) {
+                    main.uvodMenu();
+                }
+
         }
         else{
             System.out.println("Prihlasovacie meno je nespravne");
             main.uvodMenu();
-            
         }
+        scanner.close();
         
+    }
+    public static void odobrat(){
+        snackMenu snack = new snackMenu();
+        
+    }
+    public static void doplnit(){
+        snackMenu snack = new snackMenu();
     }
 }
