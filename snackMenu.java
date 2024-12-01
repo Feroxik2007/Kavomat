68d5a60c8b9a9d19640631a5494efbf17450da53
Feroxik2007 <kovalikadrian@gmail.com>
2024-12-01 19:07:29 +0100

    Update snackMenu.java

--- snackMenu.java
+++ snackMenu.java
@@ -12,11 +12,7 @@ public class snackMenu {
         Snack snacky = new Snack();
         snacky.setCena(1.9);
         
-        int snickers = 5;
-        int mms = 5;
-        int reeses = 5;
-        int Kitkat = 5;
-        int twix = 5;
+        int mnozstvo = 5;
         Scanner scanner = new Scanner(System.in);
         Map<Integer, String> map = new HashMap<>();
         map.put(1, "Snickers " );
@@ -25,24 +21,26 @@ public class snackMenu {
         map.put(4, "Kit Kat  ");
         map.put(5, "Twix" );
         System.out.println("Tu je nase menu: ");
-        System.out.println("Snickers -  [1A]" + "cena: " +snacky.getCena());
+        System.out.println("Snickers -  [1A]" + " " + "cena: " +snacky.getCena());
         Thread.sleep(500);
-        System.out.println("M&M's - [2B]" + "cena: " +  snacky.getCena());
+        System.out.println("M&M's - [2B]" + " "+ "cena: "  + snacky.getCena());
         Thread.sleep(500);
-        System.out.println("Reese's Pieces - [3C]" + "cena: " + snacky.getCena());
+        System.out.println("Reese's Pieces - [3C]" + " " + "cena: " + snacky.getCena());
         Thread.sleep(500);
-        System.out.println("Kik Kat - [4D]" + "cena: " + snacky.getCena());
+        System.out.println("Kik Kat - [4D]" + " "+ "cena: " + snacky.getCena());
         Thread.sleep(500);
-        System.out.println("Twix - [5E]"  + "cena: " + snacky.getCena());
+        System.out.println("Twix - [5E]" + " " + "cena: " + snacky.getCena());
         System.out.println("Co si prajete?");
         String odpoved = scanner.nextLine();
 
         try {
             FileWriter writer = new FileWriter("snack.txt");
             if (odpoved.contains("1")) {
-                snickers--;
-                writer.write("Pocete snickersu: " + snickers);
-                if (snickers == 0) {
+                snacky.setMnozstvo(mnozstvo);
+                mnozstvo--;
+                writer.write("Pocete snickersu: " + mnozstvo);
+                writer.close();
+                if (mnozstvo == 0) {
                     System.out.println("Snickers su vypredane!");
                     Thread.sleep(1000);
                     Diddy();
@@ -52,39 +50,47 @@ public class snackMenu {
             }
             
             if(odpoved.contains("2")){
-                mms--;
-                writer.write("Pocete M&M's: " + " " + mms);
-                if (mms == 0) {
+                snacky.setMnozstvo(mnozstvo);
+                mnozstvo--;
+                writer.write("Pocete M&M's: " + " " + mnozstvo);
+                writer.close();
+                if (mnozstvo == 0) {
                     System.out.println("M&M's su vypredane");
                 }
                 System.out.println("Vybrali ste si: " + " " + map.get(2));
                 platba();
             }
             if(odpoved.contains("3")){
-                reeses--;
-                writer.write("Pocete Reese's Pieces: " + " " + reeses);
-                if (reeses == 0) {
+                snacky.setMnozstvo(mnozstvo);
+                mnozstvo--;
+                writer.write("Pocete Reese's Pieces: " + " " + mnozstvo);
+                writer.close();
+                if (mnozstvo == 0) {
                     System.out.println("Reese's Pieces su vypredane");
                 }
                 System.out.println( "Vybrali ste si: " + " " + map.get(3));
                 platba();
             }
             if(odpoved.contains("4")){
-                Kitkat--;
-                writer.write("Pocete Kit Kat: " + " " + Kitkat);
-                if (Kitkat == 0) {
+                snacky.setMnozstvo(mnozstvo);
+                mnozstvo--;
+                writer.write("Pocet Kit Kat: " + " " + mnozstvo);
+                writer.close();
+                if (mnozstvo == 0) {
                     System.out.println("Kit Kat su vypredane");
                 }
                 System.out.println("Vybrali ste si: " + " " + map.get(4));
                 platba();
             }
             if(odpoved.contains("5")){
-                twix--;
-                writer.write("Pocete Twix: " + twix);
-                if (twix == 0) {
+                snacky.setMnozstvo(mnozstvo);
+                mnozstvo--;
+                writer.write("Pocet Twix: "+ " "  + mnozstvo);
+                writer.close();
+                if (mnozstvo == 0) {
                     System.out.println("Twix su vypredane");
                 }
-                System.out.println("Vybrali ste si" + map.get(5));
+                System.out.println("Vybrali ste si"+" " + map.get(5));
                 platba();
             }
             else{
@@ -96,7 +102,6 @@ public class snackMenu {
         } catch (Exception e) {
             System.out.println("Somehing went wrong");
         }
-       
         scanner.close();
         
     }
@@ -127,11 +132,16 @@ public class snackMenu {
                 System.out.println("Zadajte presnu ciastku aku mate: ");
                 int ciastka = scanner.nextInt();
                 double vydavok = ciastka - cena.getCena();
-                if(ciastka > 6){
+                if(ciastka > 5){
                     System.out.println("Kavomat nemoze vidat taku ciastku");
+                    Thread.sleep(1000);
+                    platba();
                 }
                 else if (ciastka < 0) {
                     System.out.println("Zadali ste zlu hodnotu");
+                    System.exit(0);
+                    Thread.sleep(1000);
+                    platba();
                 }
                 System.out.println("Tu je vas vydavok: " + vydavok + " Eur");
                 System.out.println("Snack sa pripravuje...");
